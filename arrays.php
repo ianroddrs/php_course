541a6462f384e4729061f1ad743272fc73a5ddc5
ianroddrs <ianroddrs@gmail.com>
2025-07-21 12:34:28 -0300

    continuacao de arrays

--- arrays.php
+++ arrays.php
@@ -16,10 +16,54 @@
             echo '<hr>';
             print_r($lista_frutas);
             echo '<br>';
-            echo $lista_frutas[4]
-
-
+            echo $lista_frutas[4];
+            
+            echo '<hr>';
+            
             // associativos
+            $lista_frutas = array(
+                'a' => 'Banana',
+                'b' => 'Maçã',
+                'c' => 'Morango',
+                'd' => 'Uva'
+            );
+
+            $lista_frutas['w'] = 'Abacaxi';
+
+            var_dump($lista_frutas);
+            echo '<br>';
+            echo $lista_frutas['b'];
+            
+            echo '<hr>';
+            
+            // array multidimensioanl
+            
+            $lista_coisas = [];
+            
+            $lista_coisas['frutas'] = array('Banana','Maçã','Morango','Uva');
+            $lista_coisas['pessoas'] = array('João','José','Maria','Ian');
+            echo '<pre>';
+            var_dump($lista_coisas);
+            echo '<\pre>';
+            
+            echo '<hr>';
+            
+            // pesquisa em arrays
+            // in_array(pesquisa, array) -> boolean para a existencia do que está sendo procurado.
+            // array_search() -> retorna o inice do valor pesquisado caso ele exista.
+
+            $existe = in_array('Morango', $lista_frutas);
+
+            if($existe){
+                echo 'Sim, o valor pesquisado existe no array';
+            }else{
+                echo 'Não, o valor pesquisado não existe no array';
+            }
+
+            echo '<br>';
+
+            array_search('Morango', $lista_frutas);
+            
         ?>
 
     </body>
